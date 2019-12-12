eacbb8ef5b658e1b58fd9fe1b80988354ae6dfcf
mynes <kevin.voyer.developpeur@gmail.com>
2019-12-12 03:00:49 +0100

    Implement napi active-close funtions

--- src/napi/provide-window.c
+++ src/napi/provide-window.c
@@ -1,14 +1,9 @@
 #include "wmctrl-napi.h"
-#include <stdio.h>
 
 napi_value getWindowList(napi_env env, napi_callback_info info) {
-    Display *disp;
-    if (! (disp = XOpenDisplay(NULL))) {
-        napi_throw_error(env, NULL, "Can't open dispay");
+    struct window_list *wl = list_windows(disp_client);
+    if (!wl)
         return NULL;
-    }
-
-    struct window_list *wl = list_windows(disp);
     napi_value wins_js;
 
     if (napi_create_array_with_length(env, wl->client_list_size, &wins_js) != napi_ok) {
@@ -22,21 +17,17 @@ napi_value getWindowList(napi_env env, napi_callback_info info) {
             napi_throw_error(env, NULL, "Can't create win_js");
             return NULL;
         }
-        napi_set_element(env, wins_js, i, win_js);
+        if (napi_set_element(env, wins_js, i, win_js) != napi_ok) {
+            napi_throw_error(env, NULL, "Can't set element to win_js");
+            return NULL;
+        }
     }
     free_window_list(wl);
-    XCloseDisplay(disp);
     return wins_js;
 }
 
 napi_value getActiveWindow(napi_env env, napi_callback_info info) {
-    Display *disp;
-    if (! (disp = XOpenDisplay(NULL))) {
-        napi_throw_error(env, NULL, "Can't open dispay");
-        return NULL;
-    }
-
-    struct window_info *wi = get_active_window(disp);
+    struct window_info *wi = get_active_window(disp_client);
     if (!wi) {
         napi_throw_error(env, NULL, "Unexpected error can't get active window");
         return NULL;
@@ -47,9 +38,7 @@ napi_value getActiveWindow(napi_env env, napi_callback_info info) {
         napi_throw_error(env, NULL, "Can't create window_js");
         return NULL;
     }
-
     free_window_info(wi);
-    XFree(disp);
     return win_js;
 }
 
@@ -72,13 +61,8 @@ napi_value getWindowsByPid(napi_env env, napi_callback_info info) {
         napi_throw_error(env, NULL, "Can't set input pid_js to C pid");
         return NULL;
     }
-    Display *disp;
-    if (! (disp = XOpenDisplay(NULL))) {
-        napi_throw_error(env, NULL, "Can't open dispay");        
-        return NULL;
-    }
 
-    struct window_list*wl = get_windows_by_pid(disp, pid);
+    struct window_list*wl = get_windows_by_pid(disp_client, pid);
     if (!wl) {
         napi_throw_error(env, NULL, "Unexpected error can't windows for this pid");
         return NULL;
@@ -91,21 +75,23 @@ napi_value getWindowsByPid(napi_env env, napi_callback_info info) {
     }
 
     for (size_t i = 0; i < wl->client_list_size; i++) {
-        napi_value win_js; 
+        napi_value win_js;
         if (!create_window(env, &win_js, wl->client_list + i)) {
             napi_throw_error(env, NULL, "Can't create win_js");
             return NULL;
         }
-        napi_set_element(env, wins_js, i, win_js);
+        if (napi_set_element(env, wins_js, i, win_js) != napi_ok) {
+            napi_throw_error(env, NULL, "Can't set element to win_js");
+            return NULL;
+        }
     }
     free_window_list(wl);
-    XFree(disp);
     return wins_js;
 }
 
 napi_value getWindowsByClassName(napi_env env, napi_callback_info info) {
     napi_value argv[1];
-    size_t argc = 1;    
+    size_t argc = 1;
 
     if (napi_get_cb_info(env, info, &argc, argv, NULL, NULL) != napi_ok) {
         napi_throw_error(env, NULL, "Can't get function parameters");
@@ -124,13 +110,7 @@ napi_value getWindowsByClassName(napi_env env, napi_callback_info info) {
         return NULL;
     }
 
-    Display *disp;
-    if (! (disp = XOpenDisplay(NULL))) {
-        napi_throw_error(env, NULL, "Can't open dispay");        
-        return NULL;
-    }
-
-    struct window_list*wl = get_windows_by_class_name(disp, class_name);
+    struct window_list*wl = get_windows_by_class_name(disp_client, class_name);
     if (!wl) {
         napi_throw_error(env, NULL, "Unexpected error can't windows for this class_name");
         return NULL;
@@ -151,28 +131,5 @@ napi_value getWindowsByClassName(napi_env env, napi_callback_info info) {
         napi_set_element(env, wins_js, i, win_js);
     }
     free_window_list(wl);
-    XFree(disp);
     return wins_js;
 }
-
-napi_value init_all (napi_env env, napi_value exports) {
-    napi_value getWindowListFn;
-    napi_value getActiveWindowFn;
-    napi_value getWindowsByPidFn;
-    napi_value getWindowsByClassNameFn;
-
-    napi_create_function(env, NULL, 0, getWindowList, NULL, &getWindowListFn);
-    napi_set_named_property(env, exports, "getWindowList", getWindowListFn);
-
-    napi_create_function(env, NULL, 0, getActiveWindow, NULL, &getActiveWindowFn);
-    napi_set_named_property(env, exports, "getActiveWindow", getActiveWindowFn);
-
-    napi_create_function(env, NULL, 0, getWindowsByPid, NULL, &getWindowsByPidFn);
-    napi_set_named_property(env, exports, "getWindowsByPid", getWindowsByPidFn);
-
-    napi_create_function(env, NULL, 0, getWindowsByClassName, NULL, &getWindowsByClassNameFn);
-    napi_set_named_property(env, exports, "getWindowsByClassName", getWindowsByClassNameFn);
-    return exports;
-}
-
-NAPI_MODULE(NODE_GYP_MODULE_NAME, init_all)
